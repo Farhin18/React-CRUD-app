22a9be1b979604c5fa97e64be5c9339effb94a91
FarhinMirza <farhinmirza93414@gmail.com>
2023-01-04 16:51:02 +0530

    React-CRUD-app

---
+++ src/components/Employees.js
@@ -0,0 +1,21 @@
+const Employees = [
+{
+    id:"1",
+    Name:"Farhin Mirza",
+    Age:"21"
+},
+
+{
+    id:"2",
+    Name:"Shoaib Mirza",
+    Age:"26"
+},
+
+{
+    id:"3",
+    Name:"Zuber Mirza",
+    Age:"27"
+}
+]
+
+export default Employees;
